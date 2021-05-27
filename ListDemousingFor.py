1ebe0f231f8c810ebfe08fc195abffdd8d01dcd3
devipriyak <k.devipriya20@gmail.com>
2021-05-27 14:33:02 +0530

    Add files via upload

---
+++ ListDemousingFor.py
@@ -0,0 +1,31 @@
+a = [10, 20, 30, 40, 50]#List of integers
+for i in a:#i is pointing the List Object 'a'
+  print i,
+#Size of List
+n=len(a)#5
+print("Size of list is {}".format(n))#SIze of List is 5
+
+#Access list using index through for loop
+for i in range(n):#i->0,i-->1,i-->2,i-->3...
+      print(a[i])#a[0],a[1],a[2],a[3],a[4]
+
+'''
+output
+10
+20
+30
+40
+50
+'''
+
+
+
+
+
+
+
+
+
+
+
+
